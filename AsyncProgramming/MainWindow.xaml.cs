a77d37cb939a822c59a9b18b69457713cf141a5d
alwaysprogrammer <raghav@alwaysprogrammer.in>
2020-03-06 20:43:50 +0530

    Add project files.

---
+++ AsyncProgramming/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsyncProgramming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using(WebClient webClient = new WebClient())
+            {
+                var json = webClient.DownloadString("https://cat-fact.herokuapp.com/facts");
+                dynamic timeZones = JsonConvert.DeserializeObject<RootObject>(json);
+        
+                #region bad code to get names optimize late
+                List<All> all = new List<All>(timeZones.all);
+                List<User> user = new List<User>();
+                List<Name> name = new List<Name>();
+                foreach (var item in all)
+                {
+                    user.Add(item.user);
+                }
+               
+                foreach (var u in user)
+                {
+                    if(u != null)
+                    name.Add(u.name);
+                }
+                lstbox.ItemsSource = name;
+                lstbox.DisplayMemberPath = "first";
+                lstbox.SelectedValuePath = "last";
+                #endregion
+            }
+        }   
+    }
+}
---
+++ AsyncProgramming/User.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    public class Name
+    {
+        [JsonProperty("first")]
+        public string first { get; set; }
+        [JsonProperty("last")]
+        public string last { get; set; }
+    }
+
+    public class User
+    {
+        [JsonProperty("_id")]
+        public string _id { get; set; }
+        [JsonProperty("name")]
+        public Name name { get; set; }
+    }
+
+    public class All
+    {
+        [JsonProperty("_id")]
+        public string _id { get; set; }
+        [JsonProperty("text")]
+        public string text { get; set; }
+        [JsonProperty("type")]
+        public string type { get; set; }
+        [JsonProperty("user")]
+        public User user { get; set; }
+        [JsonProperty("upvotes")]
+        public int upvotes { get; set; }
+        [JsonProperty("userUpvoted")]
+        public object userUpvoted { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<All> all { get; set; }
+    }
+}
